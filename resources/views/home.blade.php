a76da0b03016101508be7ef5597028b75389f1d6
IamPoomMi <65160208@go.buu.ac.th>
2024-03-11 20:43:21 +0700

    edithome

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    poom
                 </div>
             </div>
         </div>
