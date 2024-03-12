3fb11cb0855757eb0b58ac8d22488cef84411633
IamPoomMi <65160208@go.buu.ac.th>
2024-03-12 21:14:21 +0700

    delete route line

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,3 @@ Route::get('/home', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
