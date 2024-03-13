fd36ea6250147d0bbbbe3f0b5ecb6b9a5b07440d
IamPoomMi <65160208@go.buu.ac.th>
2024-03-13 22:20:00 +0700

    MiddlewareJsonPage

--- app/Http/Controllers/importjson.php
+++ app/Http/Controllers/importjson.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 
 class importjson extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     public function importpage(){
         return view('importview');
     }
