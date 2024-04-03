a69e5cc65644687505ffd77dcafee97be2a47361
Punnawit108 <65160108@go.buu.ac.th>
2024-04-03 22:00:53 +0700

    update(delete_collection)

--- resources/views/collection/collection.blade.php
+++ resources/views/collection/collection.blade.php
@@ -56,9 +56,40 @@
         </div>
     </div>
 </div>
+
+
+<div class="modal" id="myModal2" tabindex="-1">
+    <div class="modal-dialog">
+        <div class="modal-content" style="background-color: #136885; color: #ffffff;">
+            <div class="modal-header" style="background-color: #073B4C; color: #ffffff;">
+                <b class="logo-icon" style="display: block; margin-bottom: 7px;">
+                    <img src="{{ url('assets/assets/for-cluster6/LOGO API.png') }}" alt="homepage" class="light-logo"
+                        width="20" style="display: block;" />
+                </b>
+                <h5 class="modal-title">Delete Collection</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <!-- กรอกชื่อ Workspace -->
+                <div class="mb-3">
+                    <label for="workspaceName" class="form-label">Are you sure you want to delete ?</label>
+                    <input type="text" class="form-control" id="workspaceName"
+                        style="background-color: #0C4F65; color: #ffffff;">
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary" id="createWorkspaceBtn"
+                    style="background-color: #808080; color: #000000;">Delete</button>
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"
+                    style="background-color: #4A8BA1; color: #ffffff;">Cancel</button>
+            </div>
+        </div>
+    </div>
+</div>
 <script>
     // เลือกไอคอน bi-pencil-square
     var pencilIcon = document.querySelector('.bi.bi-pencil-square');
+
 
     // เพิ่มการฟังก์ชันเมื่อคลิกที่ไอคอน
     pencilIcon.addEventListener('click', function() {
@@ -76,7 +107,30 @@
             myModal.hide();
         }
     });
+
+
+
 </script>
+
+<script>
+    var trashicon = document.querySelector('.bi.bi-trash');
+
+    trashicon.addEventListener('click', function() {
+
+        var myModal = new bootstrap.Modal(document.getElementById('myModal2'));
+        myModal.show();
+    });
+
+    var modal = document.getElementById('myModal2');
+    modal.addEventListener('click', function(event) {
+        if (event.target === modal) {
+            // ปิด Modal โดยใช้ Bootstrap JavaScript
+            var myModal = new bootstrap.Modal(document.getElementById('myModal2'));
+            myModal.hide();
+        }
+    });
+</script>
+
 </body>
 </html>
 
