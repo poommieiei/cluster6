231ef7d7fe6fcdf8d28c0eeea8f2a1a1a4df1854
yongyae <kasama191246@gmail.com>
2024-04-03 22:17:28 +0700

    update color close

--- resources/views/collection/collection.blade.php
+++ resources/views/collection/collection.blade.php
@@ -69,7 +69,7 @@
                         width="20" style="display: block;" />
                 </b>
                 <h5 class="modal-title">Delete Collection</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
                 <!-- กรอกชื่อ Workspace -->
