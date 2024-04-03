9ffbde8d3311830673eb55b84137b384394c8cae
MinnyMind <natapohn.pat@gmail.com>
2024-04-03 19:24:33 +0700

    eiei
    บ่ได้ทำไรเลย

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,9 @@ Route::get('/viewinsert', [WorkspaceController::class , 'viewinsert']);
 Route::get('/workspace', [WorkspaceController::class , 'indexworkspace']);
 Route::post('/insertWrokspace', [WorkspaceController::class, 'insertworkspace']);
 Route::get('/deleteworkspace/{id}', [WorkspaceController::class, 'deleteworkspace'])->name('deleteworkspace');
-
+Route::get('/account',function(){
+    return view('auth.account');
+});
 
 // Route ของ Collection (ยังไม่ได้ทำ)
 
