d6e84c161edf24eff3d875560a4c89468ecae993
Punnawit108 <65160108@go.buu.ac.th>
2024-04-06 13:13:22 +0700

    update_Front

--- resources/views/collection/emptyCollection.blade.php
+++ resources/views/collection/emptyCollection.blade.php
@@ -1,7 +1,7 @@
 @extends('collection.layoutCollection')
 @section('content')
 
-<div class="container-fluid d-flex justify-content-center align-items-center" style="height: 100vh;">
+<div class="container-fluid d-flex justify-content-center align-items-center" style="height: 100vh;color:#808080;">
     <h2>Press "Import" to import new collection</h2>
 </div>
 
