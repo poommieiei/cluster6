0db3021f416fa179e68580270059a021a86e4ba8
IamPoomMi <65160208@go.buu.ac.th>
2024-04-08 23:55:39 +0700

    เกือบจะดีแต่ยัง

--- app/Http/Controllers/CollectionController.php
+++ app/Http/Controllers/CollectionController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Collection;
-use App\Models\Method;
 use App\Models\Workspace;
+use App\Models\Request;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -17,18 +17,20 @@ class CollectionController extends Controller
         $this->middleware('auth');
     }
 
-    public function indexEmptyCollection(){
+    public function indexEmptyCollection()
+    {
         return view('collection.emptyCollection');
     }
 
     public function indexcollection($id)
     {
-        $collections = Collection::where('workspace_id' , $id)->get();
+        $collections = Collection::where('workspace_id', $id)->get();
         // dd($id , $collections);
-        return view('collection.collection' , compact('id' , 'collections'));
+        return view('collection.collection', compact('id', 'collections'));
     }
 
-    public function renamecollection(Request $req , $id){
+    public function renamecollection(Request $req, $id)
+    {
         // dd($req , $id);
         $req->validate([
             'collectionName' => 'required|string|max:40'
@@ -41,15 +43,15 @@ class CollectionController extends Controller
         return redirect()->back();
     }
 
-    public function deletecollection($id){
+    public function deletecollection($id)
+    {
         DB::table('collection')->where('id', $id)->delete();
         return redirect()->back();
     }
 
-    public function importcollection(Request $request ,$id)
+    public function importcollection(Request $request, $id)
     {
 
-        // dd($request);
         if ($request->hasFile('json_file')) {
             $file = $request->file('json_file');
 
@@ -62,41 +64,51 @@ class CollectionController extends Controller
                     if ($data === null && json_last_error() !== JSON_ERROR_NONE) {
                         return back()->with('error', 'Error decoding JSON file.');
                     }
+                    dd($data);
+                    // $variable = [];
+                    // for($index = 0 ; $index < count($data['variable']) ; $index++){
 
-                    $info = $data['info']['name']; // *
-                    $method = Method::class;
-                    for( $index = 0 ; $index < count($data['item']) ; $index++){
-                        dd($data['item']);
-                        // $method = Method::create([
-                        //     '' => ,
-                        // ]);
-                        print_r($data['item'][$index]['name']);
-                        print_r($data['item'][$index]['name']);
-                    }
-
+                    // }
+                    $info = $data['info']['name'];
+                    $request = Request::class;
                     $collection = Collection::Create([
                         'collection_name' => $info,
                         'workspace_id' => $id,
                     ]);
 
-                    $item = $data['item']['name'];
+                    for ($index = 0; $index < count($data['item']); $index++) {
+                        $pathURL = $data['item'][$index];
+                        if(is_array($data['item'][$index]['request']['url'])){
+                            $request::create([
+                                'request_name' => $pathURL['name'],
+                                'method_request' => $pathURL['request']['method'],
+                                'method_url' => $pathURL['request']['method']['raw'],
+                                'collection_id' => $collection->id
+                            ]);
 
-                    if(array_key_exists('variable' , $data)){
-                        $variable = $data['variable'];
+                        }
+                        else{
+                            print_r($pathURL);
+                        }
+
                     }
 
+
+
+                    $item = $data['item']['name'];
+
+                    if (array_key_exists('variable', $data)) {
+                        $variable = $data['variable'];
+                    }
                 } catch (Exception $e) {
                     // return back()->with('error', 'Error processing JSON file.');
                 }
             } else {
                 return back()->with('error', 'The selected file is not a JSON file.');
             }
-        }
-        else {
+        } else {
             return back()->with('error', 'No file was uploaded.');
         }
         // return redirect()->back();
     }
-
-
 }
