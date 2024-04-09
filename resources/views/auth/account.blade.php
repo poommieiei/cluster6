e341f1479ac083e86d4a83e16d8c5274fa74e128
kritmet001 <152863406+kritmet001@users.noreply.github.com>
2024-04-09 21:28:29 +0700

    Update account.blade.php
    user account

--- resources/views/auth/account.blade.php
+++ resources/views/auth/account.blade.php
@@ -1,5 +1,39 @@
 @extends('layouts.layout')
 @section('title','User Account')
 @section('content')
-{{-- โค้ดตรงนี้ --}}
+<style>
+    .custom-container {
+        width: 1400px;
+        height: 320px;
+    }
+    .custom-image {
+        width: 250px;
+        margin: 0;
+    }
+    .custom-text {
+        color: floralwhite;
+        font-size: 25px;
+        padding: 10px;
+    }
+</style>
+
+<div class="container mx-4">
+    <div class="row">
+        <div class="col-md-6" style="width: 300px">
+            <div class="row">
+                <div class="col-md-6">
+                    <img src="https://static-00.iconduck.com/assets.00/user-2-account-icon-2048x2046-oucjsuyg.png" alt="Account" class="custom-image rounded-circle" />
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 py-4" style="padding: 10px">
+            <div class="row justify-content-end py-2">
+                <span class="custom-text" value="name">Username</span>
+            </div>
+            <div class="row justify-content-end py-4">
+                <span class="custom-text" value="email">Email@gmail.com</span>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
