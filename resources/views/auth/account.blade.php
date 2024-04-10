70b502728586895d5c6a204a503fa43c904a5305
TNP <65160330@go.buu.ac.th>
2024-04-10 10:08:28 +0700

    Acc User

--- resources/views/auth/account.blade.php
+++ resources/views/auth/account.blade.php
@@ -1,3 +1,42 @@
 @extends('layouts.layout')
-@section('title',"Account")
+@section('title','User Account')
 @section('content')
+<style>
+    .custom-container {
+        width: 1400px;
+        height: 320px;
+    }
+    .custom-image {
+        width: 250px;
+        margin: 0;
+    }
+    .custom-text {
+        color: floralwhite;
+        font-size: 25px;
+        padding: 10px;
+    }
+</style>
+
+<div class="container mx-4">
+    <div class="row">
+        <div class="col-md-6" style="width: 300px">
+            <div class="row">
+                <div class="col-md-6">
+                    <img src="https://static-00.iconduck.com/assets.00/user-2-account-icon-2048x2046-oucjsuyg.png" alt="Account" class="custom-image rounded-circle" />
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 py-4" style="padding: 10px">
+            <div class="row justify-content-end py-2">
+                <span class="custom-text" value="name">
+                    {{ Auth::user()->name }}</span>
+            </div>
+            <div class="row justify-content-end py-4">
+                <span class="custom-text" value="email">
+                    {{ Auth::user()->email }} </span>
+
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
